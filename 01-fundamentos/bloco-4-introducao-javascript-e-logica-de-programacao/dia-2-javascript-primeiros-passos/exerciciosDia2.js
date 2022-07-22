14d0aea3d3d3dcf94e94e38d9410890ca7653d1f
anayslima <anays.silva13@gmail.com>
2022-07-22 00:55:13 -0300

    exerciciosDia2.js

---
+++ 01-fundamentos/bloco-4-introducao-javascript-e-logica-de-programacao/dia-2-javascript-primeiros-passos/exerciciosDia2.js
@@ -0,0 +1,19 @@
+// Iremos utilizar esse array para realizar os exercícios do 1 ao 7:
+
+// Ex - 1 Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log();
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for(let i = 0; i < numbers.length; i += 1) {
+    console.log(numbers[i]);
+}
+
+//=============================================================================================================
+
+// Ex - 2 Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+
+let numbers1 = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let i = 0; i < numbers1.length; i += 1) {
+    
+}
